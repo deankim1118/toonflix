c496c4ea41914f96806eba87e006a04cc9c7fbc1
Dean Kim <deankim1118@gmail.com>
2024-10-19 21:37:08 -0400

    Get webtoon API

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
+
 class APiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonsInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonsInstances.add(instance);
+      }
+      return webtoonsInstances;
+    }
+    throw Error();
+  }
 }
